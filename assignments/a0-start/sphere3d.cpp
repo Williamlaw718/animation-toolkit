f498e283bb8227f7750c7feb06ddbf7d02aaa6e8
William Lawrence <wlawrence@haverford.edu>
2021-09-09 23:03:34 -0400

    Sphere3d animation and gif and screenshot

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,4 +1,7 @@
 #include "atkui/framework.h"
+#include <string>
+#define GLFW_KEY_SPACE   32
+#define GLFW_KEY_R   82
 
 using glm::vec3;
 
@@ -8,24 +11,31 @@ class Sphere3D : public atkui::Framework {
   }
   virtual void setup(){
     currentPos = vec3(0,0,0);
-    vel = 100.0f * vec3(1, 3, 4);
+    vel = 400.0f * vec3(1, 3, 4);
+    key = getchar();
+
   }
   virtual void scene() {
-    // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
-    //drawSphere(vec3(0), radius);
-    // draw a sphere at center of the world
-    //currentPos = vel * dt();
-    //float px = r * cos(theta) + .5 * width(); 
-    //float py = r * sin(theta) + .5 * height();
     drawSphere(vec3(currentPos), radius);
+    if(value == GLFW_KEY_SPACE){
+      keyUp(key, value);
+    }
+
+    if(value == GLFW_KEY_R){
+      keyUp(key, value);
+    }
   }
 
    void keyUp(int key, int mods) {
+
+
+
+     
     if (key == GLFW_KEY_SPACE ) {
       //std::cout << "Toggle camera controls " << cameraEnabled() << std::endl;
       //setCameraEnabled(!cameraEnabled());
-      currentPos = agl::randomUnitVector();
+      currentPos = agl::randomUnitVector() * (vel * dt());
       //drawSphere(currentPos, radius);
     }
 
@@ -38,6 +48,8 @@ class Sphere3D : public atkui::Framework {
     vec3 currentPos;
     vec3 vel;
     float radius = 50.0;
+    char key;
+    int value;
 };
 
 int main(int argc, char** argv)

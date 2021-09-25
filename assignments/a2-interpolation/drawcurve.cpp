4c8d1708139f450034349ef8e9fbcb2fbd88d379
William Lawrence <wlawrence@haverford.edu>
2021-09-24 22:17:02 -0400

    Part of HW

--- assignments/a2-interpolation/drawcurve.cpp
+++ assignments/a2-interpolation/drawcurve.cpp
@@ -17,22 +17,74 @@ class DrawCubic : public atkui::Framework
     setColor(vec3(1,1,0));
     drawSphere(B1, 10);
     drawSphere(B2, 10);
+    for(float i = 0; i < 1; i = i + step){
+       setColor(vec3(0,0,1));
+       //drawSphere(currentPosition,100);
+       if (keyIsDown(GLFW_KEY_1) == true)
+       {
+        currentPosition = BezierCurve(i);
+        currentPosition2 = BezierCurve(i + step);
+        drawLine(currentPosition,currentPosition2);
+       }
+       else if (keyIsDown(GLFW_KEY_2) == true)
+       {
+        currentPosition = BezierCurve(i);
+        currentPosition2 = BezierCurve(i + step);
+        drawLine(currentPosition,currentPosition2);
+       }
+       
+       
+       //drawLine(currentPosition,currentPosition2);
+       //t = t + .25f;
+       //currentPosition = currentPosition+iRate;
+    }
+
+    //setColor(vec3(0,0,1));
+    //drawLine(currentPosition, currentPosition2);
+    //setColor(vec3(1,0,0));
+    //drawLine(currentPosition,currentPosition2);
+    //setColor(vec3(1,0,0));
+    //drawSphere(currentPosition,100);
+    //drawLine(currentPosition, vec3(100,100,0));
   }
 
-  void keyUp(int key, int mod) {
+  /*void keyUp(int key, int mod) {
     if (key == GLFW_KEY_1) {
-      // draw Bernstein
+      currentPosition = BezierCurve(t);
+      currentPosition2 = BezierCurve(t + .025f);
+
     }
+
     else if (key == GLFW_KEY_2) {
-      // draw Casteljau
+      currentPosition = CD(t);
+      t = t + .25f;
     }
-  }
+
+  
+  }*/
+
+   vec3 BezierCurve(float t){
+      value = ((1.0f-t) * (1.0f-t) * (1.0f - t) * B0) + (3.0f*t*(1.0f-t) * (1.0f-t) * B1) + ((3*t * 3 * t)*(1-t)*B2) + ((t*t*t)*B3);
+      return value;
+    }
+
+    vec3 CD(float t){
+      return (B0 * (1.0f - t) + B1 + t ) + (B1 * (1.0f - t) + B2 + t ) + (B2 * (1.0f - t) + B3 + t );
+    }
 
  private:
   vec3 B0 = vec3(100,  50, 0);
   vec3 B1 = vec3(150, 200, 0);
   vec3 B2 = vec3(250, 100, 0);
   vec3 B3 = vec3(300, 300, 0);
+  vec3 vel = .1f * vec3(1, 3, 0);
+  float iRate = .1;
+  int g = 0; 
+  vec3 currentPosition;
+  float t = 0;
+  vec3 currentPosition2;
+  vec3 value;
+  float step = 1.0f/40.0f;
 };
 
 int main(int argc, char **argv) {
--- assignments/a2-interpolation/gradient.cpp
+++ assignments/a2-interpolation/gradient.cpp
@@ -6,10 +6,33 @@ class Gradient : public atkui::Framework {
  public:
   Gradient() : atkui::Framework(atkui::Orthographic) {
   }
-
-  virtual void scene() {
+  virtual void setup(){
+    px = 25;
+    py = 475;
   }
+  virtual void scene() {
+    setColor(CNW);
+    //drawCube(vec3(25,475,0),vec3(50,50,0));
+    for(int i = 0; i < 500; i++){
+      px = px + 25;
+      for(int j = 0; j < 500; j++){
+        drawCube(vec3(px, py,0),vec3(50,50,0));
+        py = py - 25;
+      }
+    }
+  }
+
+  private:
+  vec3 CNW = vec3(1,1,0);
+  vec3 CNE = vec3(0,1,1);
+  vec3 CSW = vec3(1,0,0);
+  vec3 CSE = vec3(1,0,1);
+  float tx;
+  float ty;
+  float px;
+  float py;
 };
+
 
 int main(int argc, char **argv) {
   Gradient viewer;
